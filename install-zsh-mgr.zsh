c4146ed701c3754391ccf5259cf988288e931825
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2024-03-16 20:13:21 +0100

    Improved installation script

--- install-zsh-mgr.zsh
+++ install-zsh-mgr.zsh
@@ -40,11 +40,63 @@ _prepend_to_file() {
     mv "$1"{.new,}
 }
 
+# $1: Time formatted using s,m,h,d,w as seconds, minutes, hours, days and weeks.
+# return: The time in seconds, an error code the format is wrong.
+parse_time(){
+    local -r TIME="$1"
+
+    # Check if the format is incorrect
+    ! echo "$TIME" | grep -iE "^[1-9]+[0-9]*[smhdw]{1}$" > /dev/null && return 1
+
+    # ${var:offset:length} for substring expansion
+    local -r TIME_MOD="${TIME: -1}"
+    local -r TIME_NUM="${TIME: 0 : -1}"
+
+    local result
+    echo "$TIME_MOD" | grep -iE "[smhdw]" > /dev/null && result="$TIME_NUM"
+    echo "$TIME_MOD" | grep -iE "[mhdw]"  > /dev/null && result=$((result*60))
+    echo "$TIME_MOD" | grep -iE "[hdw]"   > /dev/null && result=$((result*60))
+    echo "$TIME_MOD" | grep -iE "[dw]"    > /dev/null && result=$((result*24))
+    echo "$TIME_MOD" | grep -iE "[w]"     > /dev/null && result=$((result*7))
+
+    echo "$result"
+}
+
+ask_for_update_interval(){
+    echo "${GREEN}Time format examples: ${NO_COLOR}"
+    echo "${GREEN}1 second -> 1s${NO_COLOR}"
+    echo "${GREEN}1 minute -> 1m${NO_COLOR}"
+    echo "${GREEN}1 hour   -> 1h${NO_COLOR}"
+    echo "${GREEN}1 week   -> 1w${NO_COLOR}"
+    echo -e "${GREEN}############################################${NO_COLOR}\n"
+    
+
+    local error="1"
+    while [ "$error" -ne "0" ]
+    do
+        echo -n "Please input time interval to update plugins: "
+        read -r time_plugin
+        time_plugin_sec=$(parse_time "$time_plugin")
+        error="$?"
+    done
+
+    error="1"
+    while [ "$error" -ne "0" ]
+    do
+        echo -n "Please input time interval to update the manager: "
+        read -r time_mgr
+        time_mgr_sec=$(parse_time "$time_mgr")
+        error="$?"
+    done
+}
+
+# $1: Plugin time interval
+# $2: Manager time interval
 _prepend_to_config() {
     local -r PLUGIN_DIR="export ZSH_PLUGIN_DIR=\"$ZSH_PLUGIN_DIR\""
     local -r CONFIG_DIR="export ZSH_CONFIG_DIR=\"\$HOME/.config/zsh\""
-    local -r TIME1="export TIME_THRESHOLD=604800"
-    local -r TIME2="export TIME_THRESHOLD=604800"
+    local -r TIME1="export TIME_THRESHOLD=$1"
+    local -r TIME2="export MGR_TIME_THRESHOLD=$2"
     local -r SOURCE_FILE="source \$ZSH_CONFIG_DIR/zsh-mgr/zsh-mgr.zsh"
 
     _prepend_to_file "$HOME/.zshrc" "$PLUGIN_DIR\n$CONFIG_DIR\n$TIME1\n$TIME2\n\n$SOURCE_FILE\n\n"
@@ -62,31 +114,37 @@ _create_symlink() {
     ln -sf "$SCRIPT_DIR/zsh-mgr.zsh" "$ZSH_CONFIG_DIR/zsh-mgr.zsh" 
 }
 
+main(){
+    # Check check wether .zshrc exists
+    if [ ! -f "$HOME/.zshrc" ]; then
+        echo -e "$RED.zshrc file does not exist!$NO_COLOR \nPlease, create one."
+        exit 1
+    fi
 
-# Check check wether .zshrc exists
-if [ ! -f "$HOME/.zshrc" ]; then
-    echo -e "$RED.zshrc file does not exist!$NO_COLOR \nPlease, create one."
-    exit 1
-fi
+    local final_time_plugin="604800"
+    local final_time_mgr="604800"
 
+    # Check if the package manager is going to be installed silently with default options
+    if [ "$#" -eq 0 ]; then
+        echo "Installing interactively..."
+        
+        # Interactive install
+        _interactive_install
+        ask_for_update_interval
+        final_time_plugin="$time_plugin_sec"
+        final_time_mgr="$time_mgr_sec"
 
-# Check if the package manager is going to be installed silently with default options
-if [ "$#" -eq 0 ]; then
-    echo "Installing interactively..."
-    
-    # Interactive install
-    _interactive_install
+    # Quiet installation
+    elif [ "$1" = "-q" ]; then
+        echo "Installing quietly..."
+    else
+        echo "Unrecognized parameter"
+        exit 1
+    fi
 
-# Quiet installation
-elif [ "$1" = "-q" ]; then
-    echo "Installing quietly..."
-else
-    echo "Unrecognized parameter"
-    exit 1
-fi
+    # Create the directories and prepend the lines to the config file
+    _create_directories
+    _prepend_to_config "$final_time_plugin" "$final_time_mgr"
+}
 
-
-# Create the directories and prepend the lines to the config file
-_create_directories
-_prepend_to_config
-# _create_symlink
+# main "$@"
